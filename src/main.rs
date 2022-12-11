a0e05c09ad9657124e15935c8ef546245cfb19cf
Iris Pupo <pupoi@tutanota.com>
2022-12-11 00:43:31 -0500

    kinda reads tcp stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+use std::io::BufReader;
+
+fn handle_client(stream: TcpStream) -> std::io::Result<()>{
+    let mut buf:Vec<u8> = vec![0; 512];
+    let mut tcp_data = BufReader::new(stream.try_clone().unwrap());
+    tcp_data.read_to_end(&mut buf)?;
+    let request = String::from_utf8(buf).unwrap();
+    eprintln!("{request}");
+
+    //fix this to make sure that [1] exists before getting it
+    let path: &str = request.split(' ').collect::<Vec<&str>>()[1];
+    eprintln!("{path}");
+    
+    Ok(())
+}
+
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6969")?;
+
+    for stream in listener.incoming(){
+        handle_client(stream?)?;
+    }
+    Ok(())
 }
