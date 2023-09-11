ecaf0094af5f9ae851abea0b0c9406875879ff37
lilith <78992082+lilithium-hydride@users.noreply.github.com>
2023-09-10 21:12:32 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use async_std::task::spawn;
 use mime_guess::mime::TEXT_PLAIN_UTF_8;
 use simdutf8::compat::from_utf8;
 use std::{
+    env,
     fs::{self, File},
     io::{BufRead, BufReader, BufWriter, Read, Write},
     net::{TcpListener, TcpStream},
     path::{Component, PathBuf},
     str::FromStr,
-    env,
 };
 use strum_macros::EnumString;
-use tap::{Tap, Pipe};
+use tap::{Pipe, Tap};
 
 #[derive(Debug, EnumString)]
 enum HttpRequestType {
@@ -57,12 +57,15 @@ trait ToHttpError<T, E: std::fmt::Debug> {
     fn to_http_error(self, error_type: HttpErrorCode) -> Result<T, HttpErrorCode>;
 }
 
-impl<T, E> ToHttpError<T, E> for Result<T, E> where E: std::fmt::Debug {
+impl<T, E> ToHttpError<T, E> for Result<T, E>
+where
+    E: std::fmt::Debug,
+{
     fn to_http_error(self, error_type: HttpErrorCode) -> Result<T, HttpErrorCode> {
         match self {
             Ok(t) => Ok(t),
             Err(e) => {
-                e.tap_dbg(|e| eprintln!("Creating error {:?} due to {:?}",error_type , e));
+                e.tap_dbg(|e| eprintln!("Creating error {:?} due to {:?}", error_type, e));
                 Err(error_type)
             }
         }
@@ -90,10 +93,10 @@ impl HttpErrorCode {
             version: "HTTP/1.1".into(),
             status_code,
             status_text: status_text.into(),
-            headers: vec![format!{"Content-Length: {}", status_text.len() + 4}],
+            headers: vec![format! {"Content-Length: {}", status_text.len() + 4}],
             //3 characters for the code itself and one for the space
             content_length: status_text.len() as u32 + 4,
-            body: format!{"{status_code} {status_text}"}.into_bytes(),
+            body: format! {"{status_code} {status_text}"}.into_bytes(),
         }
     }
 }
@@ -109,9 +112,12 @@ impl HttpResponse {
                 self.status_text,
                 self.headers.join("\r\n"),
                 "Content-Length: ".to_owned() + &self.content_length.to_string(),
-            ).as_bytes(),
-            &*self.body, b"\r\n\r\n"
-        ].concat();
+            )
+            .as_bytes(),
+            &*self.body,
+            b"\r\n\r\n",
+        ]
+        .concat();
     }
 
     // Returns self formatted as an HTTP response, excluding binary data.
@@ -124,7 +130,7 @@ impl HttpResponse {
             self.headers.join("\r\n"),
             "Content-Length: ".to_owned() + &self.content_length.to_string(),
             from_utf8(&*self.body).unwrap_or("Binary data")
-        )
+        );
     }
 }
 
@@ -137,43 +143,51 @@ async fn handle_client(request: String, cwd: String) -> HttpResult<HttpResponse>
         // Create a vec for the 3 fields in the first line of the HTTP request header
         let request = request
             .split(' ')
-            .take(3)  // Immediately defeats every malformed request attack
-            .map(|x| x.trim())  // Remove extraneous line breaks and whatnot
+            .take(3) // Immediately defeats every malformed request attack
+            .map(|x| x.trim()) // Remove extraneous line breaks and whatnot
             .collect::<Vec<&str>>();
 
         // Shove our request vec into a struct
         HttpRequest {
-            req_type: HttpRequestType::from_str(request.get(0)
-                .ok_or(HttpErrorCode::BadRequest)?)
+            req_type: HttpRequestType::from_str(request.get(0).ok_or(HttpErrorCode::BadRequest)?)
                 .unwrap_or(HttpRequestType::UNKNOWN),
-            resource: request.get(1)
-                .ok_or( HttpErrorCode::BadRequest)?.to_string(),
-            version: request.get(2)
-                .ok_or( HttpErrorCode::BadRequest)?.to_string()
+            resource: request.get(1).ok_or(HttpErrorCode::BadRequest)?.to_string(),
+            version: request.get(2).ok_or(HttpErrorCode::BadRequest)?.to_string(),
         }
-    }.tap_dbg(|x| eprintln!("\nREQUEST:\n{:#?}", x));
+    }
+    .tap_dbg(|x| eprintln!("\nREQUEST:\n{:#?}", x));
 
     // Read file into `body` buffer, and fetch length and MIME type
     let mut body = vec![];
-    let path =
-        PathBuf::from(cwd.clone() + &match request.resource.as_str() {
-            path if path.ends_with("/") => request.resource + "/" + &"index.html".to_string(),
-            _ => request.resource,
-        })
-            // Don't allow path traversal exploits
-            .tap_dbg(|x| eprintln!("\nResolving path:\n    Unprocessed: {:?}", x))
-            .canonicalize()
-            .to_http_error(HttpErrorCode::NotFound)?
-            .tap_dbg(|x| eprintln!("    Canonicalized: {:?}", x))
-            .pipe(|x| if x.starts_with(cwd.clone()) {x} else {PathBuf::new()})
-            .tap_dbg(|x| eprintln!("    Resolved path: {:?}", x));
+    let path = PathBuf::from(
+        cwd.clone()
+            + &match request.resource.as_str() {
+                path if path.ends_with("/") => request.resource + "/" + &"index.html".to_string(),
+                _ => request.resource,
+            },
+    )
+    // Don't allow path traversal exploits
+    .tap_dbg(|x| eprintln!("\nResolving path:\n    Unprocessed: {:?}", x))
+    .canonicalize()
+    .to_http_error(HttpErrorCode::NotFound)?
+    .tap_dbg(|x| eprintln!("    Canonicalized: {:?}", x))
+    .pipe(|x| {
+        if x.starts_with(cwd.clone()) {
+            x
+        } else {
+            PathBuf::new()
+        }
+    })
+    .tap_dbg(|x| eprintln!("    Resolved path: {:?}", x));
 
     //TODO make this return proper errors on all paths
-    let length = File::open(&path).to_http_error(HttpErrorCode::NotFound)?
-        .read_to_end(&mut body).to_http_error(HttpErrorCode::InternalServerError)?;
+    let length = File::open(&path)
+        .to_http_error(HttpErrorCode::NotFound)?
+        .read_to_end(&mut body)
+        .to_http_error(HttpErrorCode::InternalServerError)?;
 
     let mime = mime_guess::from_path(&path)
-        .first()  // Assume the first MIME guess is right
+        .first() // Assume the first MIME guess is right
         .unwrap_or(TEXT_PLAIN_UTF_8)
         .tap_dbg(|x| eprintln!("\nMIME at path: {}", x));
 
@@ -182,14 +196,14 @@ async fn handle_client(request: String, cwd: String) -> HttpResult<HttpResponse>
         version: "HTTP/1.1".to_string(),
         status_code: 200,
         status_text: "Success".to_string(),
-        headers: vec![format!{"Content-Type: {mime}"}],
+        headers: vec![format! {"Content-Type: {mime}"}],
         content_length: length as u32,
-        body
-    }.tap_dbg(|x| eprintln!("\nRESPONSE:\n{}", x.to_string())))
-     // .to_data(&mut response);
+        body,
+    }
+    .tap_dbg(|x| eprintln!("\nRESPONSE:\n{}", x.to_string())))
+    // .to_data(&mut response);
 
     // stream_write.write_all(&mut response).to;
-
 }
 
 #[async_std::main]
@@ -200,7 +214,8 @@ async fn main() -> std::io::Result<()> {
     if let Ok(cwd) = env::var("PWD") {
         env::set_current_dir(cwd).expect("no enviorment variable for path");
     }
-    let cwd = PathBuf::from(env::current_dir().expect("Failed to get working directory")).into_os_string();
+    let cwd = PathBuf::from(env::current_dir().expect("Failed to get working directory"))
+        .into_os_string();
     let cwd = cwd.to_string_lossy().to_string();
 
     for stream in listener.incoming() {
@@ -214,9 +229,10 @@ async fn main() -> std::io::Result<()> {
             let mut stream_write = BufWriter::new(&stream);
 
             let mut response: Vec<u8> = vec![];
-            result.unwrap_or_else(|x| x.to_http_response()).to_data(&mut response);
+            result
+                .unwrap_or_else(|x| x.to_http_response())
+                .to_data(&mut response);
             stream_write.write(&response).unwrap();
-            
         });
     }
     Ok(())
